cfd7b592bbc00d1c3314399daac87aba46849bf5
Himanshi Gulati <himanshigulati07@gmail.com>
2021-12-28 17:41:17 -0600

    added navigation bar and routing

--- src/app/app.material.ts
+++ src/app/app.material.ts
@@ -3,10 +3,20 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
+        MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule,
+        MatListModule],
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,
+         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule,
+         MatListModule]
 })
 
 export class MaterialModule {}
